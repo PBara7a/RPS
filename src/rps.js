945098c2fd332f1c81cd84614cd52c22941c532d
Paulo Barata <pbara7a@gmail.com>
2022-04-19 21:29:46 +0100

    Function to decide winner of one round.

--- src/rps.js
+++ src/rps.js
@@ -1,14 +1,31 @@
 const plays = {
   rock: 0,
   paper: 1,
-  scissors: 2
+  scissors: 2,
+  0: 'Rock',
+  1: 'Paper',
+  2: 'Scissors'
 }
 
-function computerPlay() {
+function computerSelectionValue() {
   return Math.floor(Math.random() * 3);
 }
 
-function playerSelection() {
-  const play = prompt("Rock, Paper or Scissors?").trim().toLowerCase()
-  return plays[play]
+function playerSelectionValue() {
+  const play = prompt("Rock, Paper or Scissors?").trim().toLowerCase();
+  return plays[play];
+}
+
+function playRound(playerValue, computerValue) {
+  const result = (3 + computerValue - playerValue) % 3;
+  switch (result) {
+    case 2:
+      window.alert(`You Win! ${plays[playerValue]} beats ${plays[computerValue]}`);
+      break;
+    case 1:
+      window.alert(`You Lose! ${plays[computerValue]} beats ${plays[playerValue]}`);
+      break;
+    default:
+      window.alert(`It is a tie!`);
+  }
 }
