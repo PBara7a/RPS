dafe88a4e9761dee577cb07b549eb86218762486
Paulo Barata <pbara7a@gmail.com>
2022-04-19 21:44:23 +0100

    Function to play a game with multuple rounds.

--- src/rps.js
+++ src/rps.js
@@ -7,6 +7,9 @@ const plays = {
   2: 'Scissors'
 }
 
+let playerPoints = 0;
+let computerPoints = 0;
+
 function computerSelectionValue() {
   return Math.floor(Math.random() * 3);
 }
@@ -20,12 +23,24 @@ function playRound(playerValue, computerValue) {
   const result = (3 + computerValue - playerValue) % 3;
   switch (result) {
     case 2:
+      playerPoints++;
       window.alert(`You Win! ${plays[playerValue]} beats ${plays[computerValue]}`);
       break;
     case 1:
+      computerPoints++
       window.alert(`You Lose! ${plays[computerValue]} beats ${plays[playerValue]}`);
       break;
     default:
       window.alert(`It is a tie!`);
   }
+}
+
+function playGame(numberOfRoundsToWin) {
+  do {
+    const player1 = playerSelectionValue();
+    const computer = computerSelectionValue()
+    playRound(player1, computer)
+  } while (playerPoints < numberOfRoundsToWin && computerPoints < numberOfRoundsToWin)
+  
+  playerPoints > computerPoints ? window.alert(`You won!`) : window.alert(`You lost!`);
 }
