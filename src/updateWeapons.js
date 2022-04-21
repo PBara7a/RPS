9d62c3275d31d648e35e43c7ad98b6547b9695cf
Paulo Barata <pbara7a@gmail.com>
2022-04-21 19:16:39 +0100

    Refactored updateWeapons.

--- src/updateWeapons.js
+++ src/updateWeapons.js
@@ -4,6 +4,6 @@ import { gameData } from './gameData.js'
 export default function updateWeapons(playerValue, computerValue) {
   const playerImg = gameData.plays[playerValue].toLowerCase()
   const computerImg = gameData.plays[computerValue].toLowerCase()
-  playerWeapon.setAttribute('src', `/img/${playerImg}.png`)
-  computerWeapon.setAttribute('src', `/img/${computerImg}.png`)
+  playerWeapon.src = `img/${playerImg}.png`
+  computerWeapon.src = `img/${computerImg}.png`
 }
