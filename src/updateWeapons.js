bddc2a1d071d9065d742e728b1872022babdbd08
Paulo Barata <pbara7a@gmail.com>
2022-04-21 19:22:05 +0100

    links..

--- src/updateWeapons.js
+++ src/updateWeapons.js
@@ -4,6 +4,6 @@ import { gameData } from './gameData.js'
 export default function updateWeapons(playerValue, computerValue) {
   const playerImg = gameData.plays[playerValue].toLowerCase()
   const computerImg = gameData.plays[computerValue].toLowerCase()
-  playerWeapon.src = `img/${playerImg}.png`
-  computerWeapon.src = `img/${computerImg}.png`
+  playerWeapon.src = `RPS/img/${playerImg}.png`
+  computerWeapon.src = `RPS/img/${computerImg}.png`
 }
