bbb2c88623dd0aef3f1fbeae5ce74bbd7049c279
Paulo Barata <pbara7a@gmail.com>
2022-04-21 19:12:32 +0100

    Cleaned console log.

--- src/rps.js
+++ src/rps.js
@@ -6,14 +6,11 @@ import { gameData } from "./gameData.js"
 const options = document.querySelectorAll('.option')
 options.forEach((option) => {
   option.addEventListener('click', (e) => {
-    console.log(e)
     let player
-    if (e.target.alt) {
-      player = gameData.plays[e.target.alt]
-    } else {
+    e.target.alt ?
+      player = gameData.plays[e.target.alt] :
       player = gameData.plays[e.srcElement.childNodes[1].alt]
-    }
-    // player = gameData.plays[e.target.alt]  || gameData.plays[e.srcElement.childNodes[1].alt]
+
     const computer = computerSelectionValue()
     playRound(player, computer)
   })
