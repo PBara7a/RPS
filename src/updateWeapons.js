5a3ceddbd7602bc21d7bb02d2dd4c03adb152bec
Paulo Barata <pbara7a@gmail.com>
2022-04-21 19:10:43 +0100

    FIX: correct weapons loading when the board is updated.

--- src/updateWeapons.js
+++ src/updateWeapons.js
@@ -2,6 +2,8 @@ import { playerWeapon, computerWeapon } from './UI.js'
 import { gameData } from './gameData.js'
 
 export default function updateWeapons(playerValue, computerValue) {
-  playerWeapon.setAttribute('src', `/img/${gameData.plays[playerValue]}.png`)
-  computerWeapon.setAttribute('src', `/img/${gameData.plays[computerValue]}.png`)
+  const playerImg = gameData.plays[playerValue].toLowerCase()
+  const computerImg = gameData.plays[computerValue].toLowerCase()
+  playerWeapon.setAttribute('src', `/img/${playerImg}.png`)
+  computerWeapon.setAttribute('src', `/img/${computerImg}.png`)
 }
